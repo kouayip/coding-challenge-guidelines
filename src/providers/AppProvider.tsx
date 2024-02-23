6a428dd5b02b418011f5a5ba1a28447b6972f3a0
Yves Kams <kamwayves37@gmail.com>
2024-02-23 03:45:55 +0100

    add error limit for application provider

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -4,9 +4,7 @@ import { ErrorBoundary } from 'react-error-boundary'
 import ErrorFallback from '../pages/misc/ErrorFallback'
 
 const AppProvider: React.FC<React.PropsWithChildren> = ({ children }) => (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      {children}
-    </ErrorBoundary>
+  <ErrorBoundary FallbackComponent={ErrorFallback}>{children}</ErrorBoundary>
 )
 
 export default AppProvider
