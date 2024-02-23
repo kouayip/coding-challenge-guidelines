448b0e60a5539c73234c64c15c86f5596f1c1540
Yves Kams <kamwayves37@gmail.com>
2024-02-23 05:06:14 +0100

    export types ans valeur constants

--- src/components/SocialPostChart/index.ts
+++ src/components/SocialPostChart/index.ts
@@ -1,1 +1,5 @@
-export { default as SocialPostChart } from "./SocialPostChart";
+export { default } from './SocialPostChart'
+export { default as SocialPostChart } from './SocialPostChart'
+
+export * from './SocialPostChart.types.ts'
+export * from './constants.ts'
